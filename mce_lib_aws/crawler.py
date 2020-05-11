9209d811973d0513cc2b92c40fcd1481d7149b5e
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-05-11 18:19:01 +0200

    remote set/unset tags and refactoring crawler

--- mce_lib_aws/crawler.py
+++ mce_lib_aws/crawler.py
@@ -1,28 +1,39 @@
+from typing import List, Dict
 import logging
 from datetime import date, datetime
 from decimal import Decimal
 
 import boto3
+from boto3.session import Session as BotoSession
 
 from . import _AVAILABLES_RESOURCES
 from .common import Asset
 
 logger = logging.getLogger(__name__)
 
-__all__ = ('get_cloud_assets', 'set_asset_tags')
+__all__ = [
+    'get_cloud_assets'
+]
 
 
-def get_credentials(account_id, delegation_role_name):
-    # Get credential of the delagated role over STS
-    role_arn = 'arn:aws:iam::{}:role/{}'.format(
-        account_id, delegation_role_name)
-    sts_client = boto3.client('sts')
+def get_credentials(account_id, delegation_role_name, session=None):
+    # Get credential of the delegated role over STS
+
+    role_arn = f'arn:aws:iam::{account_id}:role/{delegation_role_name}'
+
+    if session:
+        sts_client = session.client('sts')
+    else:
+        sts_client = boto3.client('sts')
+
     assumed_role_object = sts_client.assume_role(
         DurationSeconds=900,
         RoleArn=role_arn,
         RoleSessionName='AssumeRoleSession1'
     )
+
     credentials = assumed_role_object['Credentials']
+
     return dict(
         aws_access_key_id=credentials['AccessKeyId'],
         aws_secret_access_key=credentials['SecretAccessKey'],
@@ -48,79 +59,75 @@ def clean(value):
         return Decimal(str(value))
     return value
 
+def get_cloud_assets(
+        session: BotoSession,
+        account_id: str,
+        resources_allowed: List = None, resources_exclude: List = None,
+        regions_allowed: List = None, regions_exclude: List = None,
+        endpoint_url: str = None) -> (str, str, Dict):
+    """Get all assets in an AWS Account
 
-def get_cloud_assets(account_id, delegation_role_name, resources_allowed, regions_allowed):
-    """Get all assets in an AWS Account"""
+    # example:
+    >>> import boto3
+    >>> from mce_lib_aws.crawler import get_cloud_assets
+    >>> account_id = '123456789'
+    >>> regions_allowed = ['eu-west-3', 'aws-global']
+    >>> resources_allowed = ['aws.s3.bucket', 'aws.ec2.instance']
+    >>> session = boto3.Session(aws_access_key_id="xxx", aws_secret_access_key="xxx", region_name="eu-west-3")
+    >>> for resource_type, region, asset in get_cloud_assets(session, account_id, resources_allowed=resources_allowed, regions_allowed=regions_allowed):
+    >>>     print(resource_type, region, dict(asset._asdict()))
 
-    aws_creds = get_credentials(account_id, delegation_role_name)
+    # With role delegation:
+    # Connect with AWS-CLI or set env variables before this:
+    >>> import boto3
+    >>> from mce_lib_aws.crawler import get_cloud_assets
+    >>> account_id = '123456789'
+    >>> regions_allowed = ['eu-west-3', 'aws-global']
+    >>> resources_allowed = ['aws.s3.bucket', 'aws.ec2.instance']
+    >>> aws_creds = get_credentials(account_id, delegation_role_name)
+    >>> session = boto3.Session(**aws_creds)
+    >>> for resource_type, region, asset in get_cloud_assets(session, account_id, resources_allowed=resources_allowed, regions_allowed=regions_allowed):
+    >>>     print(resource_type, region, dict(asset._asdict()))
+    """
 
-    # Open a session to get Account Alias, then later available regions
-    session = boto3.session.Session(**aws_creds)
-    client = session.client('iam')
-    account_alias = client.list_account_aliases()['AccountAliases'][0]
+    for resource_type, klass in _AVAILABLES_RESOURCES.items():
 
-    logger.info('Gather resources on AWS Account', extra=dict(data=dict(
-        account_alias=account_alias,
-        account_id=account_id
-    )))
-
-    for resource_name, generator in _AVAILABLES_RESOURCES.items():
-        # Skip not allowed resources
-        if resources_allowed and resource_name not in resources_allowed:
+        if resources_allowed and resource_type not in resources_allowed:
+            continue
+        if resources_exclude and resource_type in resources_exclude:
             continue
 
-        # Botocore knows for each resources which regions are available
-        # KB: https://github.com/boto/botocore/blob/master/botocore/data/endpoints.json
         regions_available = session.get_available_regions(
-            service_name=generator.boto_service_name,
+            service_name=klass.boto_service_name,
             allow_non_regional=True
         )
 
         for region in regions_available:
-            # Skip not allowed regions
+
             if regions_allowed and region not in regions_allowed:
                 continue
+            if regions_exclude and region in regions_exclude:
+                continue
+
             # Skip special regions
             if region == 'local' or region[:4] == 'fips':
                 continue
 
-            for asset in generator(region=region, account=account_id, session=session):
+            for asset in klass(
+                    region=region,
+                    account=account_id,
+                    session=session,
+                    endpoint_url=endpoint_url):
 
-                # Remove unsupported types in DynamoDB
                 asset_tags = clean(asset.tags)
                 asset_data = clean(asset.data)
                 asset_data['Tags'] = None
 
-                # A namedtuple is readonly, reset its values with a new one
                 asset = Asset(arn=asset.arn,
                               data=asset_data,
                               tags=asset_tags,
                               name=asset.name)
 
-                # Dot not inventory instances in AutoScaling
-                if resource_name == 'aws.ec2.instance' and asset.tags.get('aws:autoscaling:groupName'):
-                    continue
-
-                yield resource_name, region, asset
+                yield resource_type, region, asset
 
 
-def set_asset_tags(account_id, delegation_role_name, region, resource_name, asset_id, tags):
-    aws_creds = get_credentials(account_id, delegation_role_name)
-    session = boto3.session.Session(**aws_creds)
-
-    resource_cls = _AVAILABLES_RESOURCES[resource_name]
-    asset = resource_cls(region=region,
-                         account=account_id,
-                         session=session)
-    asset.set_tags(arn=asset_id, tags_dict=tags)
-
-
-def unset_asset_tags(account_id, delegation_role_name, region, resource_name, asset_id, tag_keys):
-    aws_creds = get_credentials(account_id, delegation_role_name)
-    session = boto3.session.Session(**aws_creds)
-
-    resource_cls = _AVAILABLES_RESOURCES[resource_name]
-    asset = resource_cls(region=region,
-                         account=account_id,
-                         session=session)
-    asset.unset_tags(arn=asset_id, keys=tag_keys)
