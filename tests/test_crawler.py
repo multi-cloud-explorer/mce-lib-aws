395937cdb910b4a0121cc1231119f80aadcad6d9
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-05-11 18:19:38 +0200

    update crawler test

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,20 +1,21 @@
 from moto import mock_iam, mock_sts, mock_apigateway
 
-from mce_lib_aws.crawler import get_cloud_assets
+import boto3
+from mce_lib_aws.crawler import get_cloud_assets, get_credentials
 
 @mock_iam
 @mock_sts
 @mock_apigateway
-def test_crawler(apigateway_restapi, aws_session, aws_region, aws_account_id, aws_delegation_role_name):
+def test_crawler(apigateway_restapi, aws_session, aws_region, aws_account_id):
 
     arn = apigateway_restapi(aws_session, aws_region)
 
-    iam_client = aws_session.client('iam', region_name=aws_region)
-    iam_client.create_account_alias(AccountAlias='mock-account')
+    #iam_client = aws_session.client('iam', region_name=aws_region)
+    #iam_client.create_account_alias(AccountAlias='mock-account')
 
     assets_cloud = get_cloud_assets(
+        session=aws_session,
         account_id=aws_account_id,
-        delegation_role_name=aws_delegation_role_name,
         resources_allowed=['aws.apigateway.restapis'],
         regions_allowed=[aws_region]
     )
@@ -28,19 +29,28 @@ def test_crawler(apigateway_restapi, aws_session, aws_region, aws_account_id, aw
     assert 'us-east-1' == region
     assert arn == asset.arn
 
-"""
-    assets_cloud = get_cloud_assets(account_id='123456789012',
-                                    delegation_role_name='LinkbynetInventoryRole',
-                                    resources_allowed=['aws.acm.certificate'],
-                                    regions_allowed=['us-east-1'])
+@mock_iam
+@mock_sts
+@mock_apigateway
+def test_crawler_with_role_delegation(apigateway_restapi, aws_session, aws_region, aws_account_id, aws_delegation_role_name):
 
+    arn = apigateway_restapi(aws_session, aws_region)
+
+    creds = get_credentials(aws_account_id, aws_delegation_role_name)
+    session = boto3.Session(region_name=aws_region, **creds)
+
+    assets_cloud = get_cloud_assets(
+        session=session,
+        account_id=aws_account_id,
+        resources_allowed=['aws.apigateway.restapis'],
+        regions_allowed=[aws_region]
+    )
 
     inventory_list = list(assets_cloud)
-    self.assertEqual(1, len(inventory_list))
+
+    assert len(inventory_list) == 1
 
     resource_name, region, asset = inventory_list[0]
-    self.assertEqual('aws.acm.certificate', resource_name)
-    self.assertEqual('us-east-1', region)
-    self.assertEqual(self.arn, asset.arn)
-
-"""
+    assert 'aws.apigateway.restapis' == resource_name
+    assert 'us-east-1' == region
+    assert arn == asset.arn
